5c64fbb8df34ff2c05e3277851ea97c5ce107d1c
deNataschaKlein <natascha-klein@outlook.de>
2023-02-17 14:56:22 +0100

    wip(project): Projekt aufsetzen SEP-0

---
+++ sepproject/src/pages/index.tsx
@@ -0,0 +1,23 @@
+import { supabase } from '../../lib/supabaseClient'
+
+function Home({ study_programs }) {
+  return (
+      <ul>
+        {study_programs.map((studyprogram) => (
+            <li key={studyprogram.id}>{studyprogram.name}</li>
+        ))}
+      </ul>
+  );
+}
+
+export async function getServerSideProps() {
+  let { data } = await supabase.from('study_programs').select()
+
+  return {
+    props: {
+      study_programs: data
+    },
+  }
+}
+
+export default Home;
