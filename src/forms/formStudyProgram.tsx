1080e931e5df4b55ea9270cd9aafab383c411d64
Johannes <>
2023-04-05 13:22:29 +0200

    inital commit

--- src/forms/formStudyProgram.tsx
+++ src/forms/formStudyProgram.tsx
@@ -1,17 +1,151 @@
-import React, { useState } from "react";
+import { supabase } from "../../lib/supabaseClient";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Form.module.css";
 
 export default function FormStudyProgram({ addStudy }) {
   const [studyProgram, setStudyProgram] = useState("");
-  const [specialization, setSpecialisation] = useState("");
   const [studyType, setStudyType] = useState("");
 
-  const handleSubmit = (e) => {
+  const [programs, setPrograms] = useState<any[]>([]);
+  const [name, setName] = useState("");
+  const [specialization, setSpecialization] = useState("");
+  const [programWI, setProgramWI] = useState<any[]>([]);
+  const [programBWL, setProgramBWL] = useState<any[]>([]);
+  const [programAI, setProgramAI] = useState<any[]>([]);
+  const [formError, setFormError] = useState(null);
+  const [formSuccess, setFormSuccess] = useState(null);
+
+  async function getStudyPrograms() {
+    let { data: study_programs } = await supabase
+      .from("study_programs")
+      .select("*");
+    setPrograms(study_programs);
+  }
+
+  async function getStudyProgramWI() {
+    let { data: study_programs } = await supabase
+      .from("study_programs")
+      .select("*")
+      .in("name", ["Wirtschaftsinformatik"])
+      .eq("id", 1);
+    setProgramWI(study_programs);
+  }
+
+  async function getStudyProgramBWL() {
+    let { data: study_programs } = await supabase
+      .from("study_programs")
+      .select("*")
+      .in("name", ["Betriebswirtschaftslehre"])
+      .eq("id", 2);
+    setProgramBWL(study_programs);
+  }
+
+  async function getStudyProgramAI() {
+    let { data: study_programs } = await supabase
+      .from("study_programs")
+      .select("*")
+      .in("name", ["Angewandte Informatik"])
+      .eq("id", 3);
+    setProgramAI(study_programs);
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!specialization || !name) {
+      setFormError("❌ Bitte gebe einen Studiengang an!");
+      return;
+    }
+
+    let { data: study_programs, error } = await supabase
+      .from("study_programs")
+      .insert([{ name, specialization }]);
+
+    if (error) {
+      console.log(error);
+      setFormError("❌ Bitte gebe einen Studiengang an!");
+    }
+    if (specialization && name) {
+      console.log(study_programs);
+      setFormSuccess("✅ Studiengang angelegt");
+    }
+  };
+
+  useEffect(() => {
+    getStudyPrograms();
+    getStudyProgramWI();
+    getStudyProgramBWL();
+    getStudyProgramAI();
+  }, []);
+
+  /*
+    const handleSubmit = (e) => {
     addStudy([{ studyProgram, specialization, studyType }]);
     e.preventDefault();
   };
+  */
 
   return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input
+          className={styles.button}
+          type="text"
+          id="specialization"
+          placeholder="Spezialisierung Studiengang hinzufügen"
+          value={specialization}
+          onChange={(e) => setSpecialization(e.target.value)}
+        />
+      </label>
+      <label>
+        {programWI.map((program) => (
+          <>
+            <p>{program.name}</p>
+            <input
+              type="radio"
+              id="2"
+              name="options"
+              value={program.name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+          </>
+        ))}
+      </label>
+      <label>
+        {programBWL.map((program, index) => (
+          <>
+            <p>{program.name}</p>
+            <input
+              type="radio"
+              id="2"
+              name="options"
+              key={index}
+              value={program.name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+          </>
+        ))}
+      </label>
+      <label>
+        {programAI.map((program, index) => (
+          <>
+            <p>{program.name}</p>
+            <input
+              type="radio"
+              id="3"
+              name="options"
+              key={index}
+              value={program.name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+          </>
+        ))}
+      </label>
+      <button onSubmit={handleSubmit} className={styles.form__submitButton}>Anlegen</button>
+      {formError && <p className="error">{formError}</p>}
+      <p>{formSuccess}</p>
+    </form>
+
+    /*
     <form
       className={styles.col__two}
       onSubmit={(e) => {
@@ -57,5 +191,6 @@ export default function FormStudyProgram({ addStudy }) {
 
       <div>Evaluierungsbögen</div>
     </form>
+    */
   );
 }
--- src/pages/studyPrograms/studyPrograms.tsx
+++ src/pages/studyPrograms/studyPrograms.tsx
@@ -11,12 +11,6 @@ function StudyPrograms(this: any) {
   const [studyModal, setStudyModal] = useState(false);
   const [name, setName] = useState("");
   const [studies, setStudies] = useState([]);
-  const [specialization, setSpecialization] = useState("");
-  const [programWI, setProgramWI] = useState<any[]>([]);
-  const [programBWL, setProgramBWL] = useState<any[]>([]);
-  const [programAI, setProgramAI] = useState<any[]>([]);
-  const [formError, setFormError] = useState(null);
-  const [formSuccess, setFormSuccess] = useState(null);
 /*  const degree = ["bachelor", "master"]*/
 
   async function getStudyPrograms() {
@@ -26,59 +20,8 @@ function StudyPrograms(this: any) {
     setPrograms(study_programs);
   }
 
-  async function getStudyProgramWI() {
-    let { data: study_programs } = await supabase
-      .from("study_programs")
-      .select("*")
-      .in("name", ["Wirtschaftsinformatik"])
-      .eq("id", 1);
-    setProgramWI(study_programs);
-  }
-
-  async function getStudyProgramBWL() {
-    let { data: study_programs } = await supabase
-      .from("study_programs")
-      .select("*")
-      .in("name", ["Betriebswirtschaftslehre"])
-      .eq("id", 2);
-    setProgramBWL(study_programs);
-  }
-
-  async function getStudyProgramAI() {
-    let { data: study_programs } = await supabase
-      .from("study_programs")
-      .select("*")
-      .in("name", ["Angewandte Informatik"])
-      .eq("id", 3);
-    setProgramAI(study_programs);
-  }
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!specialization || !name) {
-      setFormError("❌ Bitte gebe einen Studiengang an!");
-      return;
-    }
-
-    let { data: study_programs, error } = await supabase
-      .from("study_programs")
-      .insert([studies]);
-
-    if (error) {
-      console.log(error);
-      setFormError("❌ Bitte gebe einen Studiengang an!");
-    }
-    if (specialization && name) {
-      console.log(study_programs);
-      setFormSuccess("✅ Studiengang angelegt");
-    }
-  };
-
   useEffect(() => {
     getStudyPrograms();
-    getStudyProgramWI();
-    getStudyProgramBWL();
-    getStudyProgramAI();
   }, []);
 
   const addStudy = (study) => {
@@ -135,66 +78,6 @@ function StudyPrograms(this: any) {
             <FormStudyProgram addStudy={addStudy} />
           </ModalOffCanvas>
         )}
-        <form onSubmit={handleSubmit}>
-          <label>
-            <input
-              className={styles.submitInput}
-              type="text"
-              id="specialization"
-              placeholder="Spezialisierung Studiengang hinzufügen"
-              value={specialization}
-              onChange={(e) => setSpecialization(e.target.value)}
-            />
-          </label>
-          <label>
-            {programWI.map((program) => (
-              <>
-                <p>{program.name}</p>
-                <input
-                  type="radio"
-                  id="2"
-                  name="options"
-                  value={program.name}
-                  onChange={(e) => setName(e.target.value)}
-                ></input>
-              </>
-            ))}
-          </label>
-          <label>
-            {programBWL.map((program, index) => (
-              <>
-                <p>{program.name}</p>
-                <input
-                  type="radio"
-                  id="2"
-                  name="options"
-                  key={index}
-                  value={program.name}
-                  onChange={(e) => setName(e.target.value)}
-                ></input>
-              </>
-            ))}
-          </label>
-          <label>
-            {programAI.map((program, index) => (
-              <>
-                <p>{program.name}</p>
-                <input
-                  type="radio"
-                  id="3"
-                  name="options"
-                  key={index}
-                  value={program.name}
-                  onChange={(e) => setName(e.target.value)}
-                ></input>
-              </>
-            ))}
-          </label>
-          <button className={styles.submitButton}>Anlegen</button>
-          {formError && <p className="error">{formError}</p>}
-          <p>{formSuccess}</p>
-        </form>
-
         <div className={styles.studyProgram}>
           <div className={styles.studyProgram__swipersection}>
             <StudyProgramSwiper
