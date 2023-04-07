b5e9afa099493ab1ee29bd49472bfaea9fe1814f
deNataschaKlein <natascha-klein@outlook.de>
2023-04-07 12:41:30 +0200

    feat(Applications) Edit Status of Application SEP-50

--- src/components/BoxApplications.tsx
+++ src/components/BoxApplications.tsx
@@ -1,32 +1,40 @@
 import BoxBase from "@/components/BoxBase";
 import styles from "@/components/BoxApplication.module.css";
 import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
-import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
-import CheckOutlinedIcon from '@mui/icons-material/CheckOutlined';
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import CheckOutlinedIcon from "@mui/icons-material/CheckOutlined";
 
 const BoxApplications = (props) => {
-  return(
+  return (
     <>
       <BoxBase>
-        <p className="primary"> {props.name}</p>
-        <div className={styles.list}>{props.studyProgram && (
-          <span className={styles.listElement}><RocketLaunchIcon color={"primary"} /> {props.studyProgram}</span>
-        )}
-          {props.specialization && (
-            <span className={styles.listElement}><InfoOutlinedIcon color={"primary"} /> {props.specialization}</span>
-          )}
-          {props.cv && (
-            <span className={styles.listElement}><CheckOutlinedIcon color={"primary"} /> Lebenslauf</span>
-          )}
-          {props.image && (
-            <span className={styles.listElement}><CheckOutlinedIcon color={"primary"} /> Foto hochgeladen</span>
-          )}
-
-        </div>
+          <p className="primary"> {props.name}</p>
+          <div className={styles.list}>
+            {props.studyProgram && (
+              <span className={styles.listElement}>
+                <RocketLaunchIcon color={"primary"} /> {props.studyProgram}
+              </span>
+            )}
+            {props.specialization && (
+              <span className={styles.listElement}>
+                <InfoOutlinedIcon color={"primary"} /> {props.specialization}
+              </span>
+            )}
+            {props.cv && (
+              <span className={styles.listElement}>
+                <CheckOutlinedIcon color={"primary"} /> Lebenslauf
+              </span>
+            )}
+            {props.image && (
+              <span className={styles.listElement}>
+                <CheckOutlinedIcon color={"primary"} /> Foto hochgeladen
+              </span>
+            )}
+          </div>
+          {props.children}
       </BoxBase>
     </>
-  )
-
+  );
 };
 
-export default BoxApplications
+export default BoxApplications;
--- src/pages/applications/index.tsx
+++ src/pages/applications/index.tsx
@@ -1,18 +1,22 @@
 import { NextPage } from "next";
 import styles from "./applications.module.css";
 import BoxApplications from "@/components/BoxApplications";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { supabase } from "../../../lib/supabaseClient";
+import { Box, Button, Modal } from "@mui/material";
 
 const Applications: NextPage = () => {
   const [applications, setApplications] = useState<any[]>([]);
-  const [studyProgram, setStudyProgram] = useState<any[]>([])
+  const [editApplitcation, seteditApplitcation] = useState<any[]>([]);
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = () => setOpen(false);
 
   //Load Applications
   async function getApplications() {
     let { data: applications, error } = await supabase
-      .from('applications')
-      .select('*')
+      .from("applications")
+      .select("*");
     setApplications(applications);
   }
 
@@ -29,60 +33,90 @@ const Applications: NextPage = () => {
 
   useEffect(() => {
     getApplications();
-    console.log(applications)
+    console.log(applications);
   }, []);
 
-  const sendApplications = applications.filter((application) => application.status === 1)
-  const workApplications = applications.filter((application) => application.status === 2)
-  const readyApplications = applications.filter((application) => application.status === 3 || application.status === 4)
+  const sendApplications = applications.filter(
+    (application) => application.status === 1
+  );
+  const workApplications = applications.filter(
+    (application) => application.status === 2
+  );
+  const readyApplications = applications.filter(
+    (application) => application.status === 3 || application.status === 4
+  );
 
-  console.log(sendApplications)
+  function editApplication(application) {
+    setOpen(true);
+    seteditApplitcation(application);
+  }
 
+  async function startEditing() {
+    setOpen(false)
+
+    const { error } = await supabase
+      .from('applications')
+      .update({ status: 2 })
+      .eq('id', editApplitcation.id)
+
+    window.location.reload()
+  }
 
   return (
     <>
       <h1>Bewerbungen verwalten</h1>
-        <div className={styles.applications}>
-          {/*Incoming Applications*/}
+      <div className={styles.applications}>
+        {/*Incoming Applications*/}
 
-          <div className={styles.container}>
-            {sendApplications.map((application, _index) => (
+        <div className={styles.container}>
+          {sendApplications.map((application, _index) => (
+            <div onClick={() => editApplication(application)}>
               <BoxApplications
-                name={application.firstname + ' ' + application.name}
-                studyProgram={'studyProgram.name'}
-                specialization={'studyProgram.specialization'}
+                name={application.firstname + " " + application.name}
+                studyProgram={"studyProgram.name"}
+                specialization={"studyProgram.specialization"}
                 cv={true}
                 image={true}
               />
-            )) }
-
-
-          </div>
-
-          <div className={styles.container}>
-            {workApplications.map((application, _index) => (
-              <BoxApplications
-                name={application.firstname + ' ' + application.name}
-                studyProgram={'studyProgram.name'}
-                specialization={'studyProgram.specialization'}
-                cv={true}
-                image={true}
-              />
-            )) }
-          </div>
-          <div className={styles.container}>
-            {readyApplications.map((application, _index) => (
-              <BoxApplications
-                name={application.firstname + ' ' + application.name}
-                studyProgram={'studyProgram.name'}
-                specialization={'studyProgram.specialization'}
-                cv={true}
-                image={true}
-              />
-            )) }
-          </div>
+            </div>
+          ))}
         </div>
 
+        <div className={styles.container}>
+          {workApplications.map((application, _index) => (
+            <BoxApplications
+              name={application.firstname + " " + application.name}
+              studyProgram={"studyProgram.name"}
+              specialization={"studyProgram.specialization"}
+              cv={true}
+              image={true}
+            />
+          ))}
+        </div>
+        <div className={styles.container}>
+          {readyApplications.map((application, _index) => (
+            <BoxApplications
+              name={application.firstname + " " + application.name}
+              studyProgram={"studyProgram.name"}
+              specialization={"studyProgram.specialization"}
+              cv={true}
+              image={true}
+            />
+          ))}
+        </div>
+      </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box className={styles.box__modal}>
+          <h2>Bewerbung bearbeiten</h2>
+          <p>Möchtest du mit der Bearbeitung der Bewerbung von {editApplitcation.firstname + ' ' + editApplitcation.name} starten?</p>
+          <Button variant={"contained"} onClick={startEditing}>Ja</Button>
+        </Box>
+      </Modal>
     </>
   );
 };
