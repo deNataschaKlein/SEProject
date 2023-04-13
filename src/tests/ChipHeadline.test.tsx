c3f388df043b73a75b7b598af6d42b20e7f1e039
deNataschaKlein <natascha-klein@outlook.de>
2023-04-13 12:17:40 +0200

    feat(jest) install JEST suit SEP-0

---
+++ src/tests/ChipHeadline.test.tsx
@@ -0,0 +1,12 @@
+import ChipHeadline from "../components/ChipHeadline";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { expect, test } from "@jest/globals";
+import { string } from "prop-types";
+
+/*test("render Component", async () => {
+  render(<ChipHeadline label={"ein Label"} />);
+  expect(screen.getByTestId("label")).toEqual(
+    expect.stringContaining("ein Label")
+  );
+});*/
