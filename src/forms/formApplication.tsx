b4105e483e84e1a3802b806a663ec191d7b7ee7f
deNataschaKlein <natascha-klein@outlook.de>
2023-04-17 16:11:31 +0200

    feat(ApplicantID) get status of application on new site SEP-61

--- src/forms/formApplication.tsx
+++ src/forms/formApplication.tsx
@@ -1,8 +1,10 @@
 import styles from "../styles/Form.module.css";
+import applicationStyles from "../pages/applications/applications.module.css";
 import { supabase } from "../../lib/supabaseClient";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Box, Button, Modal } from "@mui/material";
+import Link from "next/link";
 import UploadDocuments from "@/components/UploadDocuments";
-import { Button } from "@mui/material";
 
 export default function FormApplication(props: any) {
   const [pdfUrl, setPdfUrl] = useState('')
@@ -32,6 +34,16 @@ export default function FormApplication(props: any) {
   const [email, setEmail] = useState<string | undefined>(undefined);
   const [document_url, setDocument_url] = useState<string | undefined>(undefined);
 
+  const [open, setOpen] = React.useState(false);
+  const [applicantID, setApplicantID] = useState<string>();
+
+  const url =
+    window.location.protocol +
+    "//" +
+    window.location.host +
+    "/applications/" +
+    applicantID;
+
   async function getStudyName() {
     let { data: study_name, error } = await supabase
       .from("study_name")
@@ -55,6 +67,7 @@ export default function FormApplication(props: any) {
     );
 
     setStudy_programs(study?.id);
+    setStudyNameID(study?.study_name);
   }
 
   async function postApplication() {
@@ -66,7 +79,7 @@ export default function FormApplication(props: any) {
       telefone &&
       studyNameID
     ) {
-      const { error } = await supabase.from("applications").insert({
+      const { data, error } = await supabase.from("applications").insert({
         firstname,
         name,
         study_programs: study_programs,
@@ -75,11 +88,20 @@ export default function FormApplication(props: any) {
         status: 1,
         studyName: studyNameID,
         document_url: document_url,
-      });
+      }).select("id");;
 
       if (error) {
-      } else window.location.reload();
+      } else {
+        setApplicantID(data[0].id);
+        setOpen(true);
+      }
     }
+  }
+
+  function dataCopy() {
+    const url = document.getElementById("url");
+
+    console.log(url);
   }
 
   useEffect(() => {
@@ -99,7 +121,7 @@ export default function FormApplication(props: any) {
     if (studiengang) {
       setStudyPrograms(studiengang);
     }
-  }, [selectedStudyProgram?.study_name, studyProgramName]);
+  }, [selectedStudyProgram, studyProgramName]);
 
   useEffect(() => {
     if (props.current) {
@@ -107,76 +129,98 @@ export default function FormApplication(props: any) {
     }
   }, [allStudy_programs, getSpecialization, props]);
   return (
-    <form className={styles.col__two}>
-      {/*Auswahl des Studiengangs vorausgewählt oder über den globalen Button*/}
-      <div>
-        <label>
-          Studiengang
-          {selectedStudyProgram ? (
-            <input
-              type={"text"}
-              value={study_programsName?.name}
-              readOnly
-            ></input>
-          ) : (
-            <select onChange={(e) => setStudyNameID(e.target.value)}>
-              <option value="none" selected disabled hidden />
-              {studyProgramName?.map((name, _index) => (
-                <option key={name.id} value={name.id}>
-                  {name.name}
-                </option>
-              ))}
-            </select>
-          )}
-        </label>
-        <label>
-          Schwerpunkt
-          {selectedStudyProgram && specialization ? (
-            <input
-              type={"text"}
-              readOnly
-              value={specialization}
-              onLoad={() => getSpecialization(specialization)}
-            />
-          ) : (
-            <select onChange={(e) => setStudy_programs(e.target.value)}>
-              <option value="none" selected disabled hidden />
-              {allStudy_programs
-                ?.filter((studyId: any) => studyId.study_name == studyNameID)
-                .map((specialization: any, _index: any) => (
-                  <option key={specialization.id} value={specialization.id}>
-                    {specialization.specialization}
+    <div>
+      <form className={styles.col__two}>
+        {/*Auswahl des Studiengangs vorausgewählt oder über den globalen Button*/}
+        <div>
+          <label>
+            Studiengang
+            {selectedStudyProgram ? (
+              <input
+                type={"text"}
+                value={study_programsName?.name}
+                readOnly
+              ></input>
+            ) : (
+              <select onChange={(e) => setStudyNameID(e.target.value)}>
+                <option value="none" selected disabled hidden />
+                {studyProgramName?.map((name, _index) => (
+                  <option key={name.id} value={name.id}>
+                    {name.name}
                   </option>
                 ))}
-            </select>
-          )}
-        </label>
+              </select>
+            )}
+          </label>
+          <label>
+            Schwerpunkt
+            {selectedStudyProgram && specialization ? (
+              <input
+                type={"text"}
+                readOnly
+                value={specialization}
+                onLoad={() => getSpecialization(specialization)}
+              />
+            ) : (
+              <select onChange={(e) => setStudy_programs(e.target.value)}>
+                <option value="none" selected disabled hidden />
+                {allStudy_programs
+                  ?.filter((studyId: any) => studyId.study_name == studyNameID)
+                  .map((specialization: any, _index: any) => (
+                    <option key={specialization.id} value={specialization.id}>
+                      {specialization.specialization}
+                    </option>
+                  ))}
+              </select>
+            )}
+          </label>
 
-        {/*  Persönliche Angaben zu der Bewerbung*/}
-        <h2>persönliche Angaben</h2>
-        <label>
-          Vorname
-          <input type={"text"} onChange={(e) => setFirstname(e.target.value)} />
-        </label>
-        <label>
-          Nachname
-          <input type={"text"} onChange={(e) => setName(e.target.value)} />
-        </label>
-        <label>
-          Email
-          <input type="email" onChange={(e) => setEmail(e.target.value)} />
-        </label>
-        <label>
-          Telefonnummer
-          <input type="text" onChange={(e) => setTelefone(e.target.value)} />
-        </label>
+          {/*  Persönliche Angaben zu der Bewerbung*/}
+          <h2>persönliche Angaben</h2>
+          <label>
+            Vorname
+            <input
+              type={"text"}
+              onChange={(e) => setFirstname(e.target.value)}
+            />
+          </label>
+          <label>
+            Nachname
+            <input type={"text"} onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            Email
+            <input type="email" onChange={(e) => setEmail(e.target.value)} />
+          </label>
+          <label>
+            Telefonnummer
+            <input type="text" onChange={(e) => setTelefone(e.target.value)} />
+          </label>
 
-        <UploadDocuments onUpload={setDocument_url} />
+          <UploadDocuments onUpload={setDocument_url} />
 
-        <Button onClick={() => postApplication()} variant={"contained"}>
-          Jetzt Bewerben
-        </Button>
-      </div>
-    </form>
+          <Button onClick={() => postApplication()} variant={"contained"}>
+            Jetzt Bewerben
+          </Button>
+        </div>
+      </form>
+      <Modal open={open}>
+        <Box className={applicationStyles.box__modal}>
+          <h2>Bewerbung wurde übermittelt</h2>
+          <div>
+            <p>
+              kopiere dir folgenden Link, um den Status deiner Bewerbung
+              jederzeit aufzurufen{" "}
+            </p>
+            <a id="url" href={url}>
+              {url}
+            </a>
+          </div>
+          <Link target="_blank" href={url}>
+            <Button> Status abrufen</Button>
+          </Link>
+        </Box>
+      </Modal>
+    </div>
   );
 }
