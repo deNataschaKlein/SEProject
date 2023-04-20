7c4d996807ce30d879677a6bdaedf2f2a2ac786d
deNataschaKlein <natascha-klein@outlook.de>
2023-04-20 14:19:19 +0200

    feat(Modals) bug fixes SEP-0

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import {
-  SessionContextProvider,
-  Session,
-  useSession,
-} from "@supabase/auth-helpers-react";
-import Account from "@/components/Account";
-import { Auth } from "@supabase/auth-ui-react";
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import styles from "@/styles/Home.module.css";
-import * as AiIcons from "react-icons/ai";
 import React, { useState } from "react";
-import Link from "next/link";
-import Login from "@/components/Login";
 import NavBar from "@/components/NavBar";
 
 export default function App({
@@ -23,9 +14,6 @@ export default function App({
   initialSession: Session;
 }>) {
   const [supabase] = useState(() => createBrowserSupabaseClient());
-  const [navBar, setNavBar] = useState(false);
-  const showNav = () => setNavBar(!navBar);
-  const session = useSession();
 
   const customTheme = createTheme({
     palette: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,40 @@
 import styles from "../styles/Home.module.css";
 import { Button } from "@mui/material";
 import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
-import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
 import Link from "next/link";
+import { useSession } from "@supabase/auth-helpers-react";
+import { useEffect } from "react";
 function Home() {
   const name = "NO_NAME";
+  const session = useSession();
+
+  useEffect(() => {
+    if (!session) {
+      window.location.href = "studyPrograms";
+    }
+  }, [session]);
 
   return (
     <>
-      <h1>Hallo {name}</h1>
+      {session && (
+        <div>
+          <h1>Hallo {name}</h1>
+          <div className={styles.box}>
+            <Link href="/applications" className={styles.nav__li}>
+              <Button variant="text" startIcon={<AssignmentIndIcon />}>
+                Zu den Bewerbungen
+              </Button>
+            </Link>
+          </div>
+        </div>
+      )}
+
       <div className={styles.home}>
         <div className={styles.box}>
           <Link href="/studyPrograms" className={styles.nav__li}>
             <Button variant="text" startIcon={<RocketLaunchIcon />}>
               Zu den Studiengängen
-            </Button>
-          </Link>
-        </div>
-        <div className={styles.box}>
-          <Link href="/applications" className={styles.nav__li}>
-            <Button variant="text" startIcon={<AssignmentIndIcon />}>
-              Zu den Bewerbungen
             </Button>
           </Link>
         </div>
